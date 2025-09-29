99e22172f6894a4148308aa2576a76c3fab5b503
skmoniri <skmoniri83@gmail.com>
2025-09-29 20:38:59 +0330

    fourth commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,34 @@ export default function Home() {
       </div>
 
       <div className="w-full bg-amber-50 h-[600px]">
-        <div className="flex justify-between pt-[50px] mx-auto w-[1800px] ">
-          <div className="bg-white flex-col h-[400px] max-w-[550px]">
-            <h1 className=" flex justify-center">img</h1>
-            <h1 className="font-bold flex justify-center">ORDER ON THE GO</h1>
-            <p className="flex justify-center">I&apos;m a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
+        <div className="flex justify-evenly pt-[50px] mx-auto max-w-[1800px] gap-18 px-5">
+          
+          <div className="bg-white flex-col h-[425px] max-w-[500px] min-w-[200px] flex justify-center">
+            <h1 className=" flex justify-center font-bold text-5xl pb-10">img</h1>
+            <h1 className="font-bold text-xl flex justify-center pb-5 ">ORDER ON THE GO</h1>
+            <p className="flex justify-center items-center mx-5 ">I&apos;m a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
           </div>
-          <div className="bg-white flex-col h-[400px] max-w-[550px]">
-            <h1 className=" flex justify-center">img</h1>
-            <h1 className="font-bold flex justify-center">ORDER ON THE GO</h1>
-            <p className="flex justify-center">I&apos;m a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
+
+          <div className="bg-white flex-col h-[425px] max-w-[500px] min-w-[200px] flex justify-center">
+            <h1 className=" flex justify-center font-bold text-5xl pb-10">img</h1>
+            <h1 className="font-bold text-xl flex justify-center pb-5 ">ORDER ON THE GO</h1>
+            <p className="flex justify-center items-center mx-5 ">I&apos;m a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
           </div>
-          <div className="bg-white flex-col h-[400px] max-w-[550px]">
-            <h1 className=" flex justify-center">img</h1>
-            <h1 className="font-bold flex justify-center">ORDER ON THE GO</h1>
-            <p className="flex justify-center">I&apos;m a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
+
+          <div className="bg-white flex-col h-[425px] max-w-[500px] min-w-[200px] flex justify-center">
+            <h1 className=" flex justify-center font-bold text-5xl pb-10">img</h1>
+            <h1 className="font-bold text-xl flex justify-center pb-5 ">ORDER ON THE GO</h1>
+            <p className="flex justify-center items-center mx-5 ">I&apos;m a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
           </div>
         </div>
+      </div>
+      
+      <div className="w-full bg-amber-50 h-[300px]">
+        <h1 className="text-red-500 font-bold text-7xl w-[600px] mx-auto text-center"> ORIGINAL RECIPE SINCE 2035</h1>
+      </div>
+
+      <div className=" w-full bg-red-50 h-[700px]">
+        <h1>ALL TIME FAVORITES</h1>
       </div>
     </>
   );
